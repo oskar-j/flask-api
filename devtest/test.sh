b7d51014c474210b94afa4192972f706747510bf
Oskar Jarczyk <oskar.jarczyk@pja.edu.pl>
2018-05-28 01:47:26 +0200

    variables should be rather exported, not set

--- devtest/test.sh
+++ devtest/test.sh
@@ -2,19 +2,19 @@
 
 echo "Configuring windows variables for the Flask APP"
 
-FLASK_APP=app.py
+export FLASK_APP=app.py
 echo FLASK_APP value set to $FLASK_APP
 
-DATABASE_URL=postgresql://dev:WFwt8UDpkfUpRsVS@localhost/dev
+export DATABASE_URL=postgresql://dev:WFwt8UDpkfUpRsVS@localhost/dev
 echo DATABASE_URL value set to $DATABASE_URL
 
-DEVTEST_CONF=DEVEL
+export DEVTEST_CONF=DEVEL
 echo DEVTEST_CONF value set to $DEVTEST_CONF
 
-IP_ADDRESS=127.0.0.1
+export IP_ADDRESS=127.0.0.1
 echo IP_ADDRESS value set to $IP_ADDRESS
 
-PORT=5001
+export PORT=5001
 echo PORT value set to $PORT
 
 nose2 -v
