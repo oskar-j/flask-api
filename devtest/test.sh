6d30a12c26534411ce922bb722f673a7ade6ddab
Oskar Jarczyk <oskar.jarczyk@pja.edu.pl>
2018-05-27 02:09:21 +0200

    Unix has different db user name

--- devtest/test.sh
+++ devtest/test.sh
@@ -5,7 +5,7 @@ echo "Configuring windows variables for the Flask APP"
 FLASK_APP=app.py
 echo FLASK_APP value set to $FLASK_APP
 
-DATABASE_URL=postgresql://postgres:WFwt8UDpkfUpRsVS@localhost/dev
+DATABASE_URL=postgresql://dev:WFwt8UDpkfUpRsVS@localhost/dev
 echo DATABASE_URL value set to $DATABASE_URL
 
 DEVTEST_CONF=DEVEL
